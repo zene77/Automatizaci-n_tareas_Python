84ff6af9484e267f0d499b2317c35cef02306c05
Zene <42480271+zene77@users.noreply.github.com>
2026-05-12 12:21:24 +0200

    Añadir generación de PDF con ReportLab

--- prueba_pdf.py
+++ prueba_pdf.py
@@ -1,9 +1,11 @@
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 
 styles = getSampleStyleSheet()
 report = SimpleDocTemplate("prueba.pdf")
 
 titulo = Paragraph("Mi primer PDF", styles["h1"])
+texto = Paragraph("Este es un párrafo de prueba para practicar ReportLab.", styles["BodyText"])
+espacio = Spacer(1, 20)
 
-report.build([titulo])
+report.build([titulo, espacio, texto])
